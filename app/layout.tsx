bf5cf8e818c6644831e2cb363392df07f45d469e
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-02-21 08:42:32 -0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { client } from "@/services/apollo";
 import { ApolloProvider } from "@apollo/client";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+import Script from "next/script";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -26,6 +27,21 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+         <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GAID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_GAID}', {
+                page_path: window.location.pathname,
+              });
+            `}
+          </Script>
           <body
             className={cn(
               "min-h-screen bg-background font-sans antialiased",
