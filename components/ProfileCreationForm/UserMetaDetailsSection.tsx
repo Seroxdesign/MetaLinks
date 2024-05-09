2549ffeff339c40fe6db6d33d1b53236cb2979c0
Yash Gupta <yash3144@gmail.com>
2024-05-09 22:48:07 +0530

    style: changes looks for background image

--- components/ProfileCreationForm/UserMetaDetailsSection.tsx
+++ components/ProfileCreationForm/UserMetaDetailsSection.tsx
@@ -1,10 +1,9 @@
-"use client";
-
+import React from "react";
+import Image from "next/image";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { GradiantSeparatorLine } from "./GradiantComponents";
-
 import LabelInputContainer from "./LabelInputContainer";
+import { GradiantSeparatorLine } from "./GradiantComponents";
 
 export type TUserMetaDetails = {
   username: string;
@@ -28,33 +27,39 @@ const UserMetaDetailsSection = ({
       [field]: value,
     });
   };
+
   return (
     <>
-      <LabelInputContainer className="mb-4">
-        <Label htmlFor="username">UserName</Label>
-        <Input
-          id="username"
-          placeholder="Tyler"
-          type="text"
-          value={userMetaDetails.username}
-          onChange={(e) => handleChange("username", e.target.value)}
+      <div className="mb-8 relative">
+        <Image
+          className="rounded-lg shadow-xl dark:shadow-gray-800"
+          src={userMetaDetails.backgroundImage || "/DefaultBackgroundImage.png"}
+          alt="default background image"
+          height="376"
+          width="576"
         />
-      </LabelInputContainer>
+        <label className="absolute top-2 right-[-5px] w-10 h-10 border-white cursor-pointer">
+          <Image
+            src="/Camera.png"
+            alt="image description"
+            height="25"
+            width="25"
+          />
+          <Input
+            type="file"
+            className="hidden"
+            onChange={(e) => {
+              handleChange(
+                "backgroundImage",
+                e.target.files && e.target.files[0]
+              );
+            }}
+          />
+        </label>
+      </div>
 
-      <LabelInputContainer className="mb-4">
-        <Label htmlFor="bio">Bio</Label>
-        <Input
-          id="bio"
-          placeholder="Durden"
-          type="text"
-          value={userMetaDetails.bio}
-          onChange={(e) => handleChange("bio", e.target.value)}
-        />
-      </LabelInputContainer>
-
-      <LabelInputContainer className="mb-4">
+      <div className="mb-4">
         <Label htmlFor="profile-image">Profile image</Label>
-
         <Input
           id="profile-image"
           name="profile-image"
@@ -65,21 +70,34 @@ const UserMetaDetailsSection = ({
           }
           required
         />
-      </LabelInputContainer>
+      </div>
 
-      <LabelInputContainer className="mb-8">
-        <Label htmlFor="background-image">Background image</Label>
-        <Input
-          id="background-image"
-          type="file"
-          onChange={(e) =>
-            handleChange("backgroundImage", e.target.files && e.target.files[0])
-          }
-        />
-      </LabelInputContainer>
+      <div className="flex flex-col gap-4">
+        <LabelInputContainer>
+          <Label htmlFor="username">UserName</Label>
+          <Input
+            id="username"
+            placeholder="Tyler"
+            type="text"
+            value={userMetaDetails.username}
+            onChange={(e) => handleChange("username", e.target.value)}
+          />
+        </LabelInputContainer>
+
+        <LabelInputContainer>
+          <Label htmlFor="bio">Bio</Label>
+          <Input
+            id="bio"
+            placeholder="Durden"
+            type="text"
+            value={userMetaDetails.bio}
+            onChange={(e) => handleChange("bio", e.target.value)}
+          />
+        </LabelInputContainer>
+      </div>
 
       <button
-        className="bg-gradient-to-br right-0 absolute px-4  group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 dark:bg-zinc-800 w-fit text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+        className="bg-gradient-to-br right-0 absolute px-4 group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 dark:bg-zinc-800 w-fit text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
         onClick={onClickNextBtn}
       >
         Next &rarr;
