4868706efda068278ca6794ef424f0f98a037607
Yash Gupta <yash3144@gmail.com>
2024-06-07 19:50:26 +0530

    chore: fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,45 +1,50 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push('pino-pretty', 'encoding');
+  webpack: (config, context) => {
+    if (config.plugins) {
+      config.plugins.push(
+        new context.webpack.IgnorePlugin({
+          resourceRegExp: /^(pino-pretty)$/,
+        })
+      );
+    }
     return config;
   },
-    images: {
-      dangerouslyAllowSVG: true,
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'nft-cdn.alchemy.com',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'assets.airstack.xyz',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'metadata.ens.domains',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'w3s.link',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'ipfs.io',
-            port: '',
-            pathname: '/**',
-          },
-        ],
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nft-cdn.alchemy.com",
+        port: "",
+        pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "assets.airstack.xyz",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "metadata.ens.domains",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "w3s.link",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ipfs.io",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
